a6b91a8cb795afcfcfc10eb592c2f1d19ab706b7
rOzzy1987 <rozovits.mihaly@gmail.com>
2022-11-25 09:00:37 +0100

    Added clean and build steps to release process

---
+++ src/clean.bat
@@ -0,0 +1,2 @@
+@echo OFF
+rmdir /S /Q ByteMap\bin ByteMap\obj ByteMap.BLL\bin ByteMap.BLL\obj ByteMap.Cmd\bin ByteMap.Cmd\obj packages 2> nul
--- src/release.bat
+++ src/release.bat
@@ -2,17 +2,30 @@
 
 if "%~1"=="" goto blank
 
+call clean.bat
+
+echo Nuget restore
+dotnet restore > ByteMap.%1.log
+
+echo Build cmd
+dotnet msbuild -property:Configuration=Release ByteMap.Cmd\ByteMap.Cmd.csproj >> ByteMap.%1.log
+if %ERRORLEVEL% gtr 0 goto error
+
+echo Build WinForms
+dotnet msbuild -property:Configuration=Release ByteMap\ByteMap.csproj >> ByteMap.%1.log
+if %ERRORLEVEL% gtr 0 goto error
+
 SET FOLDER=ByteMap.%1.bin
 
 mkdir %FOLDER%
-copy /Y ByteMap\bin\Release\* %FOLDER%
-copy /Y ByteMap.Cmd\bin\Release\* %FOLDER%
-copy /Y resource\test.png %FOLDER%
+copy /Y ByteMap\bin\Release\* %FOLDER% >> nul
+copy /Y ByteMap.Cmd\bin\Release\* %FOLDER% >> nul
+copy /Y resource\test.png %FOLDER% >> nul
 cd %FOLDER%
 del *.config
 del *.xml
 del *.pdb
-7z a ..\..\..\Releases\ByteMap.%1.7z *
+7z a ..\..\..\Releases\ByteMap.%1.7z * >> nul
 cd ..
 
 rmdir /S /Q %FOLDER%
@@ -25,6 +38,13 @@ goto end
 :blank
 echo ----
 echo Set release name!
+echo ----
+goto end
+
+:error
+echo ----
+echo Error!
+echo See ByteMap.%1.log
 echo ----
 
 :end
